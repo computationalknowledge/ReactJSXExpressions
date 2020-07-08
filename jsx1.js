a23f6e6bd6053d82873fe403303f032ae75ac84c
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-07-08 12:12:07 -0400

    4
    4

--- jsx1.js
+++ jsx1.js
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom";
 
 class Greeting extends React.Component {
   render() {
-    return <h1> What going on {this.props.name}? </h1>;
+    return <h1> What's going on {this.props.firstname} ? </h1>;
+  }
+}
+
+class LikeCounter extends React.Component {
+  render() {
+    return <div>LikeCounter</div>;
   }
 }
 
@@ -20,4 +26,6 @@ const where = document.getElementById("root");
 // ReactDOM.render(what, where);
 
 // ReactDOM.render(<Greeting />, where);
-ReactDOM.render(<Greeting name="Peanut" />, where);
+ReactDOM.render(<Greeting firstname="Mike" />, where);
+
+//  ReactDOM.render(<LikeCounter />, where);
