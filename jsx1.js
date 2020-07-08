8b750caed28f971d90a5d52320285f8d48d018c9
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-07-08 10:13:53 -0400

    Update jsx1.js

--- jsx1.js
+++ jsx1.js
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom";
 
 React.createElement();
 
+const Greeting = (props) => <h1>Hello {props.name}! </h1>
+
 // const what = React.createElement("h1", {}, "Hello Peter!");
-
 // const what = <h1>Hello Peter's Class!</h1>;
+// const Greeting = () => <h1>Hello Peter's Class!</h1>;
 
-const Greeting = () => <h1>Hello Peter's Class!</h1>;
 const where = document.getElementById("root");
 
 // ReactDOM.render(what, where);
 
-ReactDOM.render(<Greeting />, where);
+// ReactDOM.render(<Greeting />, where);
+ReactDOM.render(<Greeting name="Peanut" />, where);
