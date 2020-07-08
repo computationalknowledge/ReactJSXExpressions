922a70cabe60a6340221158be3e4e75273a026d5
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-07-08 10:52:35 -0400

    Update jsx1.js

--- jsx1.js
+++ jsx1.js
@@ -1,10 +1,15 @@
 import React from "react";
-
 import ReactDOM from "react-dom";
+
+class Greeting extends React.Component {
+  render() {
+    return <h1> What going on {this.props.name}? </h1>;
+  }
+}
 
 React.createElement();
 
-const Greeting = (props) => <h1>Hello {props.name}! </h1>
+// const Greeting = (props) => <h1>Hello {props.name}! </h1>
 
 // const what = React.createElement("h1", {}, "Hello Peter!");
 // const what = <h1>Hello Peter's Class!</h1>;
